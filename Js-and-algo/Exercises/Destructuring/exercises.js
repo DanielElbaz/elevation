fe0dc19415357da46ed5fb3153d1be739ec7c380
Daniel <daniel.elbaz2000@gmail.com>
2025-07-14 23:14:47 +0300

    destructuring

--- Js-and-algo/Exercises/Destructuring/exercises.js
+++ Js-and-algo/Exercises/Destructuring/exercises.js
@@ -1,5 +1,21 @@
-let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
-let {numPlanets, yearNeptuneDiscovered} = facts;
+var firstPiece = { id: 101, name: 'Ofri' }
 
-console.log(numPlanets); // ?
-console.log(yearNeptuneDiscovered);
+var seoncdPiece = { country: 'Israel' }
+
+var passport = { ...firstPiece, ...seoncdPiece };
+console.log(passport)
+
+
+let employeesArr = [
+    { name: "Joey", id: 1, age: 26 },
+    { name: "Lily", id: null, age: 24 },
+    { name: "Alice", id: 7, age: null },
+    { name: "Sam", id: 8, age: 24 },
+    { name: "Ray", id: null, age: null }
+]
+
+for (let employe of employeesArr) {
+    if ((employe.id ?? null) === null || (employe.age ?? null) === null) {
+        console.log(`${employe.name} has missing data`);
+    }
+}
