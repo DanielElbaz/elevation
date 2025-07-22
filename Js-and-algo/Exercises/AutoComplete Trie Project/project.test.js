d49009d8cb1b16f529b0f0fad94df7be3312ec2c
Daniel <daniel.elbaz2000@gmail.com>
2025-07-22 11:14:13 +0300

    tests for each function

--- Js-and-algo/Exercises/AutoComplete Trie Project/project.test.js
+++ Js-and-algo/Exercises/AutoComplete Trie Project/project.test.js
@@ -1,7 +1,43 @@
-const AutoCompleteTrie=require('./project')
+const AutoCompleteTrie = require('./project');
 
-describe("Test for add Word:",()=>{
-    it('should add Daniel to the trie',()=>{
-        expect(AutoCompleteTrie.addWord('Daniel')).toBe("Daniel")
-    })
-})
+describe('AutoCompleteTrie', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new AutoCompleteTrie();
+  });
+
+  test('addWord should insert a word into the trie', () => {
+    trie.addWord('hello');
+    expect(trie.findWord('hello')).toBe(true);
+  });
+
+  test('findWord should return true for existing words', () => {
+    trie.addWord('world');
+    expect(trie.findWord('world')).toBe(true);
+  });
+
+  test('findWord should return false for non-existing words', () => {
+    expect(trie.findWord('banana')).toBe(false);
+  });
+
+  test('predictWords should return correct completions', () => {
+    trie.addWord('cat');
+    trie.addWord('car');
+    trie.addWord('cart');
+    trie.addWord('dog');
+    expect(trie.predictWords('ca')).toEqual(['cat', 'car', 'cart']);
+  });
+
+  test('predictWords returns empty array for unknown prefix', () => {
+    trie.addWord('one');
+    trie.addWord('only');
+    expect(trie.predictWords('xyz')).toEqual([]);
+  });
+
+  test('addWord does not duplicate existing words', () => {
+    trie.addWord('test');
+    trie.addWord('test');
+    expect(trie.predictWords('te')).toEqual(['test']); 
+  });
+});
