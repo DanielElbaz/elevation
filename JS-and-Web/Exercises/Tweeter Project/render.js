7d7b8a922e659937b933e0773737c42e489f518c
Daniel <daniel.elbaz2000@gmail.com>
2025-07-28 09:53:14 +0300

    refactor code to methods

--- JS-and-Web/Exercises/Tweeter Project/render.js
+++ JS-and-Web/Exercises/Tweeter Project/render.js
@@ -1,31 +1,46 @@
 class Render {
+    constructor() { }
+
     renderPosts(posts) {
         $("#posts").empty();
 
         for (let post of posts) {
-            const postDiv = $("<div>").addClass("post").attr("data-id", post.id);
-            const postText = $("<div>").addClass("post-text").text(post.text);
-            const deletePost = $("<div>").addClass("delete").attr("data-id", post.id).text("Delete Post");
-            postDiv.append(postText, deletePost);
+            const postDiv = this.createPostDiv(post);
 
             const commentsDiv = $("<div>").addClass("comments");
             for (let comment of post.comments) {
-                const commentDeletediv=$("<div>").addClass("comment-delete");
-                const commentDiv = $("<div>").addClass("comment").attr("data-id", comment.id).text(comment.text);
-                const deleteComment = $("<div>").addClass("delete-comment").attr("data-id", comment.id).text("X");
-                commentsDiv.append(commentDeletediv,commentDiv, deleteComment)
+                this.createCommentDiv(comment, commentsDiv);
             }
             postDiv.append(commentsDiv);
 
-            const input = $("<input>").attr("type", "text").attr("placeholder", "Got something to say?").addClass("comment-input");
-            const button = $("<button>").addClass("comment-button").text("Comment");
-            postDiv.append(input, button);
+            this.createNewTwitDiv(postDiv);
 
             $("#posts").append(postDiv);
         }
 
     }
 
+
+    createNewTwitDiv(postDiv) {
+        const input = $("<input>").attr("type", "text").attr("placeholder", "Got something to say?").addClass("comment-input");
+        const button = $("<button>").addClass("comment-button").text("Comment");
+        postDiv.append(input, button);
+    }
+
+    createCommentDiv(comment, commentsDiv) {
+        const commentDeletediv = $("<div>").addClass("comment-delete");
+        const commentDiv = $("<div>").addClass("comment").attr("data-id", comment.id).text(comment.text);
+        const deleteComment = $("<div>").addClass("delete-comment").attr("data-id", comment.id).text("X");
+        commentsDiv.append(commentDeletediv, commentDiv, deleteComment);
+    }
+
+    createPostDiv(post) {
+        const postDiv = $("<div>").addClass("post").attr("data-id", post.id);
+        const postText = $("<div>").addClass("post-text").text(post.text);
+        const deletePost = $("<div>").addClass("delete").attr("data-id", post.id).text("Delete Post");
+        postDiv.append(postText, deletePost);
+        return postDiv;
+    }
 }
 
 
