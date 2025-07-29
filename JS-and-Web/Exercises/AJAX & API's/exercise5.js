74e70f6649b51056789bce9ca95e464e6ce08bec
Daniel <daniel.elbaz2000@gmail.com>
2025-07-29 15:00:24 +0300

    exercise5 ajax

---
+++ JS-and-Web/Exercises/AJAX & API's/exercise5.js
@@ -0,0 +1,16 @@
+ 
+ 
+ 
+ document.getElementById("searchBtn").addEventListener("click", function () {
+            const query = document.getElementById("search").value;
+            const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=GZDmnGJcUw5nIhUiwcqJ9C9dJjwzndHi&limit=5`;
+
+            $.get(url).done(function (data) {
+                if (data.data.length > 0) {
+                    const gifUrl = data.data[0].embed_url;
+                    document.getElementById("gif").src = gifUrl;
+                } else {
+                    alert("no gif !");
+                }
+            });
+        });
