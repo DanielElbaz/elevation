e3fadd71b096c88805276f5a0675857db648fa15
Daniel Elbaz <99900998+DanielElbaz@users.noreply.github.com>
2025-07-29 17:55:21 +0300

    Update exercise4.js

--- JS-and-Web/Exercises/AJAX & API's/exercise4.js
+++ JS-and-Web/Exercises/AJAX & API's/exercise4.js
@@ -1,6 +1,6 @@
 //javascript, jQuery
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=GZDmnGJcUw5nIhUiwcqJ9C9dJjwzndHi&limit=5");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=&limit=5");
 xhr.done(function (data) { 
     console.log("success got data", data);
     const gif=data.data[0].embed_url
