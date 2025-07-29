6584912ce00cba8f449ed4f5e89047b1ad2d39f6
Daniel Elbaz <99900998+DanielElbaz@users.noreply.github.com>
2025-07-29 17:55:36 +0300

    Update exercise5.js

--- JS-and-Web/Exercises/AJAX & API's/exercise5.js
+++ JS-and-Web/Exercises/AJAX & API's/exercise5.js
@@ -3,7 +3,7 @@
  
  document.getElementById("searchBtn").addEventListener("click", function () {
             const query = document.getElementById("search").value;
-            const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=GZDmnGJcUw5nIhUiwcqJ9C9dJjwzndHi&limit=5`;
+            const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=&limit=5`;
 
             $.get(url).done(function (data) {
                 if (data.data.length > 0) {
