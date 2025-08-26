481ef49b2be8b804c723acc6e7dad2b650ce71e6
Daniel <daniel.elbaz2000@gmail.com>
2025-08-26 18:36:10 +0300

    exercise 4-5-6

--- Backend/Exercises/furniture-store/dist/script.js
+++ Backend/Exercises/furniture-store/dist/script.js
@@ -5,3 +5,10 @@ function Search(){
         $("body").append(`<div>${input} : ${data.price}$ </div>`)
     })
 }
+
+function Buy(){
+   let input=$("#name-input").val()
+   $.get(`/buy/${input}`,(data)=>{
+    $("body").append(`<div>Congratulations, you've just bought a ${input} for ${data.price}$. There are ${data.inventory} left now in the store. </div>`)
+   })
+}
--- Backend/Exercises/furniture-store/server.js
+++ Backend/Exercises/furniture-store/server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express();
-const path=require('path')
+const path = require('path')
 
 
-app.use(express.static(path.join(__dirname,'dist')))
+app.use(express.static(path.join(__dirname, 'dist')))
 const store = [
     { name: "table", inventory: 3, price: 800 },
     { name: "chair", inventory: 16, price: 120 },
@@ -18,12 +18,39 @@ app.get('/', (req, res) => {
 
 app.get('/priceCheck/:name', (req, res) => {
     const objectName = req.params.name;
-    const item = store.find(i => i.name.toLowerCase() === objectName) 
+    const item = store.find(i => i.name.toLowerCase() === objectName)
     if (item) {
         res.send({ price: item.price })
     } else {
         res.send({ price: null, message: "Item not found" })
     }
+})
+
+app.get('/buy/:name', (req, res) => {
+    const newName = req.params.name;
+    const item = store.find(i => i.name.toLowerCase() === newName)
+    if (!item) {
+        return res.send({ error: 'Item not found', price: null, inventory: null })
+    }
+    if (item.inventory <= 0) {
+        return res.send({ name: item.name, inventory: 0, price: item.price, message: 'Out of stock' })
+    }
+    item.inventory -= 1;
+    res.send({ name: item.name, inventory: item.inventory, price: item.price })
+
+})
+
+app.get('/sale', (req, res) => {
+    const booly = req.query.admin === "true"
+    if (booly) {
+        for (let i = 0; i < store.length; i++) {
+            if (store[i].inventory > 10) {
+                store[i].price = store[i].price / 2;
+            }
+        }
+
+    }
+    res.send(store)
 })
 
 const PORT = 8080
